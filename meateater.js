c1b3c0e606679ea4d9132896854da2aa2875b209
twfb <twfb29@yahoo.com>
2021-06-02 19:09:49 -0400

    media queries

---
+++ meateater.js
@@ -0,0 +1,7 @@
+var meateater = window.confirm("do you eat meat?")
+    if meateater == 1 {
+        window.alert("here is a hamburger");      
+    }
+    else {
+        window.alert("fuck you then");
+    }
